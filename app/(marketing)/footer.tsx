39da84a1d0e3af065db9e16f62b642fef08b0394
Siddhesh Dhomse <155984671+SiddheshDhomse@users.noreply.github.com>
2024-11-17 17:44:16 +0530

    Update footer.tsx

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import React from 'react';
 
 export const Footer = () => {
-    return(
-        <footer className=" lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            <div className="max-w-screen-lg mc-auto flex items-center justify-evenly h-full">
-            
+    return (
+        <footer className="lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                {/* Empty Footer Content */}
             </div>
         </footer>
-    )
-}
+    );
+};
