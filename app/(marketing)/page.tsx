c4fa11ce2b9a8e2fd19c2360605f493fa3451d79
Siddhesh Dhomse <155984671+SiddheshDhomse@users.noreply.github.com>
2024-11-20 12:25:02 +0530

    Update hero image page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-4">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] mb-8 lg:mb-0">
-        <Image src="/hero.svg" fill alt="hero" className="object-cover" /> 
+        <Image src="/hero.jpg" fill alt="hero" className="object-cover" /> 
       </div>
 
       <div className="flex flex-col items-center gap-y-3 max-width-[330px] w-full ">
